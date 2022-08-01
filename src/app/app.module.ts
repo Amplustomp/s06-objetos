a6a5c9d591c366452c25faafaea82709ab401b2d
Sergio (Admethius) <sergio.cp@hotmail.es>
2022-08-01 15:46:22 -0400

    push inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonaDatosComponent } from './persona/persona.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonaDatosComponent
   ],
   imports: [
     BrowserModule
